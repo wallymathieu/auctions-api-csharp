5fabfb44d2ff32298236a9712cae1f4371e68762
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2023-08-11 18:54:27 +0300

    Docs

--- src/Api/Models/IBidUserMapper.cs
+++ src/Api/Models/IBidUserMapper.cs
@@ -1,7 +1,10 @@
 using Wallymathieu.Auctions.DomainModels;
 
 namespace Wallymathieu.Auctions.Api.Models;
-
+/// <summary>
+/// The bid user mapper gives a string representation of a user id. In the case when you have a private auction that
+/// user id is the number that the user
+/// </summary>
 public interface IBidUserMapper
 {
     string? GetUserString(UserId? userId);
