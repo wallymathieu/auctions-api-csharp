98fed3cadb36d7383de874987748d76e0cab7251
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2023-12-17 18:59:58 +0200

    Use primary constructor

--- src/Auctions.Infrastructure/Cache/Services/CreateAuctionCachePipeLineBehavior.cs
+++ src/Auctions.Infrastructure/Cache/Services/CreateAuctionCachePipeLineBehavior.cs
@@ -2,19 +2,13 @@ using MediatR;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace Wallymathieu.Auctions.Infrastructure.Cache.Services;
-internal class CreateAuctionCachePipeLineBehavior:
+internal class CreateAuctionCachePipeLineBehavior(IDistributedCache cache):
     IPipelineBehavior<CreateAuctionCommand, Auction>
 {
-    private readonly IDistributedCache _cache;
-    public CreateAuctionCachePipeLineBehavior(IDistributedCache cache)
-    {
-        _cache = cache;
-    }
-
     public async Task<Auction> Handle(CreateAuctionCommand request, RequestHandlerDelegate<Auction> next, CancellationToken cancellationToken)
     {
         var res = await next();
-        await _cache.RemoveAsync(CacheKeys.Auctions, cancellationToken);
+        await cache.RemoveAsync(CacheKeys.Auctions, cancellationToken);
         return res;
     }
 }
--- src/Auctions.Infrastructure/Cache/Services/CreateBidCachePipeLineBehavior.cs
+++ src/Auctions.Infrastructure/Cache/Services/CreateBidCachePipeLineBehavior.cs
@@ -2,20 +2,13 @@ using MediatR;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace Wallymathieu.Auctions.Infrastructure.Cache.Services;
-internal class CreateBidCachePipeLineBehavior:
+internal class CreateBidCachePipeLineBehavior(IDistributedCache cache):
     IPipelineBehavior<CreateBidCommand, Result<Bid,Errors>>
 {
-    private readonly IDistributedCache _cache;
-
-    public CreateBidCachePipeLineBehavior(IDistributedCache cache)
-    {
-        _cache = cache;
-    }
-
     public async Task<Result<Bid, Errors>> Handle(CreateBidCommand request, RequestHandlerDelegate<Result<Bid, Errors>> next, CancellationToken cancellationToken)
     {
         var res = await next();
-        await _cache.RemoveAsync(CacheKeys.Auctions, cancellationToken);
+        await cache.RemoveAsync(CacheKeys.Auctions, cancellationToken);
         return res;
     }
 }
