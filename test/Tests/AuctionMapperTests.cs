b3301b790ebc867f2fdb760865c1cb292228b57a
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2024-05-10 22:21:03 +0300

    Use Verify to verify mapping

---
+++ test/Tests/AuctionMapperTests.cs
@@ -0,0 +1,28 @@
+using Wallymathieu.Auctions.Models;
+using Wallymathieu.Auctions.Tests.Helpers;
+
+namespace YourNamespace.Tests;
+
+public class AuctionMapperTests
+{
+    private readonly AuctionMapper _sut;
+
+    public AuctionMapperTests()
+    {
+        _sut = new AuctionMapper(new FakeSystemClock(DateTimeOffset.FromUnixTimeSeconds(0)));
+    }
+
+    [Fact]
+    public async Task MapAuctionToModel_ShouldReturnExpectedModel()
+    {
+        // Arrange
+        var auction = GetEnglishAuction();
+
+        // Act
+        var model = _sut.MapAuctionToModel(auction);
+
+        // Assert
+        Assert.NotNull(model);
+        await Verify(model);
+    }
+}
---
+++ test/Tests/BidMapperTests.cs
@@ -0,0 +1,24 @@
+using Wallymathieu.Auctions.Models;
+using Wallymathieu.Auctions.Tests.Helpers;
+
+namespace YourNamespace.Tests;
+
+public class BidMapperTests
+{
+    [Fact]
+    public async Task MapBidToModel_ShouldReturnExpectedModel()
+    {
+        // Arrange
+        var clock = new FakeSystemClock(new DateTime(2016, 1, 2));
+        var auction = GetEnglishAuction();
+        auction.TryAddBid(clock.Now, BidOf100, out _);
+        var sut = new BidMapper(auction, auction.BidUserMapper());
+
+        // Act
+        var model = sut.MapToBidModel(auction.GetBids(clock.Now).First());
+
+        // Assert
+        Assert.NotNull(model);
+        await Verify(model);
+    }
+}
