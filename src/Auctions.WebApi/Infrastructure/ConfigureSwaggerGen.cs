c2e8e318afd75ab4d2093f20b262aa113bf9c7b3
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2025-04-25 19:37:21 +0300

    Split

--- src/Auctions.WebApi/Infrastructure/ConfigureSwaggerGen.cs
+++ src/Auctions.WebApi/Infrastructure/ConfigureSwaggerGen.cs
@@ -1,27 +1,9 @@
 using System.Reflection;
-using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Wallymathieu.Auctions.Api;
-internal class ConfigureSwaggerUI(IApiVersionDescriptionProvider provider) :
-    IConfigureOptions<SwaggerUIOptions>
-{
-    public void Configure(SwaggerUIOptions options)
-    {
-        var descriptions = provider.ApiVersionDescriptions;
 
-        // build a swagger endpoint for each discovered API version
-        foreach (var (url,name) in from description in descriptions
-                 select (
-                     $"/swagger/{description.GroupName}/swagger.json",
-                     description.GroupName.ToUpperInvariant()))
-        {
-            options.SwaggerEndpoint(url, name);
-        }
-    }
-}
 internal class ConfigureSwaggerGen :
     IConfigureOptions<SwaggerGenOptions>
 {
---
+++ src/Auctions.WebApi/Infrastructure/ConfigureSwaggerUI.cs
@@ -0,0 +1,23 @@
+using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Wallymathieu.Auctions.Api;
+
+internal class ConfigureSwaggerUI(IApiVersionDescriptionProvider provider) :
+    IConfigureOptions<SwaggerUIOptions>
+{
+    public void Configure(SwaggerUIOptions options)
+    {
+        var descriptions = provider.ApiVersionDescriptions;
+
+        // build a swagger endpoint for each discovered API version
+        foreach (var (url,name) in from description in descriptions
+                 select (
+                     $"/swagger/{description.GroupName}/swagger.json",
+                     description.GroupName.ToUpperInvariant()))
+        {
+            options.SwaggerEndpoint(url, name);
+        }
+    }
+}
