bb28170db2799e278af30309ea5c1a57151ce91c
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2025-04-25 20:50:03 +0300

    +Test of parse failure

--- test/Tests/AuctionTypeSpec.cs
+++ test/Tests/AuctionTypeSpec.cs
@@ -14,4 +14,17 @@ public class AuctionTypeSpec
         Assert.NotNull(auctionType);
         Assert.Equal(type, auctionType.ToString());
     }
+
+    [Theory,
+     InlineData("english|1|2|3"),
+     InlineData("blind"),
+     InlineData("vickrey"),
+     InlineData("English|2|3"),
+     InlineData("Bind"),
+     InlineData("Vickre"),]
+    public void FailToParseInvalidInput(string type)
+    {
+        Assert.False(AuctionType.TryParse(type, out var auctionType));
+        Assert.Null(auctionType);
+    }
 }
